284a385ec33726f5cf41ba62e2ffc10d3e45afe9
Abdelrahman Elfatih <Nar7gamer@gmail.com>
2024-09-14 09:15:51 +0300

    just started

--- src/game.py
+++ src/game.py
@@ -13,6 +13,10 @@ class Game:
         for row in range(ROWS):
             for col in range(COLS):
                 if (row + col) % 2 == 0:
-                    color == (234, 235 , 200)
+                    color = (234, 235 , 200) # light green
                 else:
-                    color = (119, 154 , 88)
+                    color = (119, 154 , 88) # dark green
+
+                rect = (col * SQSIZE , row * SQSIZE , SQSIZE , SQSIZE)
+
+                pygame.draw.rect(surface, color, rect)
--- src/main.py
+++ src/main.py
@@ -2,15 +2,19 @@ import pygame
 import sys
 
 from const import *
+from game import Game
 
 class Main:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode( ( WIDTH , HEIGHT) )
         pygame.display.set_caption('Chess')
+        self.game = Game()
 
     def mainloop(self):
         while True:
+            self.game.show_bg(self.screen)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
