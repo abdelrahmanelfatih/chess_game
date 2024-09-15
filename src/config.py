2e5f0f9b60ec0b8807c6dd039daab0940627aec1
Abdelrahman Elfatih <Nar7gamer@gmail.com>
2024-09-15 16:17:38 +0300

    added more files

---
+++ src/config.py
@@ -0,0 +1,10 @@
+
+class Config:
+    def __init__(self):
+        pass
+
+    def change_theme(self):
+        pass
+
+    def _add_themes(self):
+        pass
---
+++ src/sound.py
@@ -0,0 +1,4 @@
+
+class Sound:
+    def __init__(self):
+        pass
