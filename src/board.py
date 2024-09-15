1c26dddfc42e254ecdddc182b6da570b77c643a1
Abdelrahman Elfatih <Nar7gamer@gmail.com>
2024-09-15 14:34:15 +0300

    added king moves

--- src/board.py
+++ src/board.py
@@ -111,6 +111,26 @@ class Board:
                 possible_move_row = possible_move_row + row_incr
                 possible_move_col = possible_move_row + col_incr
 
+        def king_moves():
+            adjs = [
+                (row-1 , col + 0) , #up
+                (row - 1, col + 1),  # up-right
+                (row + 0, col + 1),  # up
+                (row + 1, col + 1),  # down-right
+                (row + 1, col + 0),  # down
+                (row + 0, col -1),  # down-left
+                (row - 1, col + 0),  # left
+                (row - 1, col - 1),  # up-left
+            ]
+            for possible_move in adjs:
+                possible_move_row , possible_move_col = possible_move
+
+                if Square.in_range(possible_move_row , possible_move_col):
+                    if self.squares[possible_move_row][possible_move_col].isempty_or_enemy(piece.color):
+                        initial = Square(row , col)
+                        final = Square(possible_move_row , possible_move_col)
+                        move = Move(initial , final)
+                        piece.add_move(move)
 
         if isinstance(piece , Pawn):
             pawn_move()
@@ -147,7 +167,7 @@ class Board:
             ])
 
         elif isinstance(piece, King):
-            pass
+            king_moves()
 
     def _create(self):
         for row in range(ROWS):
